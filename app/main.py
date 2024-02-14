8f8ffdb4860d658fbf1a1d059843c498221be38d
Nick6154-dev <pbalsecamh5b@outlook.com>
2024-02-13 20:00:20 -0500

    First commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import asyncio
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from service.kafka_event_handler import listen_pulsar_events
+from service.email_service import EmailService
+from service.kafka_event_handler import listen_kafka_events
 
 load_dotenv()
-asyncio.create_task(listen_pulsar_events())
+email_service = EmailService()
+asyncio.create_task(listen_kafka_events())
 app = FastAPI()
